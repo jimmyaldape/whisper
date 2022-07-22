2fb51587fb4821b5b7d3f0b96a1755e78bc72769
Jimmy Aldape <jimmy@aldape.dev>
2022-07-21 23:34:26 -0700

    updates home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,34 +13,6 @@
     @vite('resources/css/app.css')
 </head>
 <body class="antialiased h-full">
-{{--@if(flash()->message)--}}
-{{--    <div class="bg-blue-600">--}}
-{{--        <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">--}}
-{{--            <div class="flex items-center justify-between flex-wrap">--}}
-{{--                <div class="w-0 flex-1 flex items-center">--}}
-{{--                        <span class="flex p-2 rounded-lg bg-blue-800">--}}
-{{--                          <!-- Heroicon name: outline/speakerphone -->--}}
-{{--                          <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">--}}
-{{--                            <path stroke-linecap="round" stroke-linejoin="round" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />--}}
-{{--                          </svg>--}}
-{{--                        </span>--}}
-{{--                    <p class="ml-3 font-medium text-white truncate">--}}
-{{--                        <span class="inline">{{ flash()->message }}</span>--}}
-{{--                    </p>--}}
-{{--                </div>--}}
-{{--                <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-3">--}}
-{{--                    <button type="button" class="-mr-1 flex p-2 rounded-md hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2">--}}
-{{--                        <span class="sr-only">Dismiss</span>--}}
-{{--                        <!-- Heroicon name: outline/x -->--}}
-{{--                        <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">--}}
-{{--                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />--}}
-{{--                        </svg>--}}
-{{--                    </button>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--@endif--}}
 
 <div class="bg-white min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
     <div class="max-w-max mx-auto">
@@ -51,14 +23,12 @@
                     <p class="mt-1 text-base text-gray-500">A laravel websocket server for the everyday man.</p>
                 </div>
                 <div class="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
-                    <a href="#" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"> Login</a>
+                    <a href="/laravel-websockets" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Dashboard</a>
                 </div>
             </div>
         </main>
     </div>
 </div>
-
-
 
 </body>
 </html>
